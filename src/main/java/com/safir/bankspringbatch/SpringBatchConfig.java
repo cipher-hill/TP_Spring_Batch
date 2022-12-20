6557befa2c8ea96d38e8dbd4a7fb8d546fbd0719
Safir Mohammed <Mohammedsafir17@gmail.com>
2022-12-21 00:02:06 +0100

    ready to start

---
+++ src/main/java/com/safir/bankspringbatch/SpringBatchConfig.java
@@ -0,0 +1,67 @@
+package com.safir.bankspringbatch;
+
+import com.safir.bankspringbatch.dao.BankTransaction;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchConfig {
+    @Autowired private JobBuilderFactory jobBuilderFactory;
+    @Autowired private StepBuilderFactory stepBuilderFactory;
+    @Autowired private ItemReader<BankTransaction> bankTransactionItemReader;
+    @Autowired private ItemWriter<BankTransaction> bankTransactionItemWriter;
+    @Autowired private ItemProcessor<BankTransaction, BankTransaction> bankTransactionItemProcessor;
+
+    @Bean
+    public Job bankJob(){
+        Step step1 = stepBuilderFactory.get("step-load-data")
+                .<BankTransaction, BankTransaction>chunk(100)
+                .reader(bankTransactionItemReader)
+                .processor(bankTransactionItemProcessor)
+                .writer(bankTransactionItemWriter)
+                .build();
+        return jobBuilderFactory.get("bank-data-loader-job")
+                .start(step1).build();
+    }
+    @Bean
+    public FlatFileItemReader<BankTransaction> flatFileItemReader(@Value("${inputFile}") Resource inputFile){
+        FlatFileItemReader<BankTransaction> fileItemReader =  new FlatFileItemReader<>();
+        fileItemReader.setName("CSV Reader");
+        fileItemReader.setLinesToSkip(1);
+        fileItemReader.setResource(inputFile);
+        fileItemReader.setLineMapper(lineMapper());
+        return  fileItemReader;
+    }
+   @Bean
+    public  LineMapper<BankTransaction> lineMapper() {
+          DefaultLineMapper<BankTransaction> lineMapper = new DefaultLineMapper<>();
+          DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+          lineTokenizer.setDelimiter(",");
+          lineTokenizer.setStrict(false);
+          lineTokenizer.setNames("id","accountID","amount","strTransactionDate");
+          lineMapper.setLineTokenizer(lineTokenizer);
+          BeanWrapperFieldSetMapper fieldSetMapper = new BeanWrapperFieldSetMapper();
+          fieldSetMapper.setTargetType(BankTransaction.class);
+          lineMapper.setFieldSetMapper(fieldSetMapper);
+          return lineMapper;
+   }
+
+}
---
+++ src/main/java/com/safir/bankspringbatch/dao/BankTransaction.java
@@ -0,0 +1,22 @@
+package com.safir.bankspringbatch.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class BankTransaction {
+    @Id
+    private Long id;
+    private long accountID;
+    private double amount;
+    private Date transactionDate;
+    @Transient
+    private String strTransactionDate;
+}
